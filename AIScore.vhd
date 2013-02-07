4855fb2170f5e39967edd9f1f657f754371b4afe
Chris <christory@hotmail.com>
2013-02-07 16:02:40 +0000

    Initial commit

---
+++ AIScore.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use work.Pack.ALL;
+
+entity AIScore is
+    Port ( CLK : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           Initial : in  InitialArray;
+           Eta : in  STD_LOGIC;
+           Result : out  STD_LOGIC);
+end AIScore;
+
+architecture Behavioral of AIScore is
+
+begin
+
+
+end Behavioral;
+
