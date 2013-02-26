f0e5366028651d117b867c23a2c0b975845b75fd
Chris <christory@hotmail.com>
2013-02-26 16:15:18 +0000

    LPR RAM sync added

--- LPR.vhd
+++ LPR.vhd
@@ -43,34 +43,39 @@ end component;
 
 	-- States
 	TYPE state_type is (idle, load_rng, running);
-	signal Proposed_Sample : pipeline_type(1 to 120);
-	signal Proposed_LPR : pipeline_type(1 to 50);
+
 	signal seed:  std_logic_vector(2047 downto 0) :="11111001100110001001100001101110011000101001101010110011001110101110110000111110000000001101101100010101100100000100110000000111100100000010000000001100011110000011001110000100100111111001001100001111010001011001001000011010111000000101011000000011100110010110110110001110000011111011110000100011110011010110010100111010110100010101110001111001110101101011010001100111011110101010011111101111101100011001110000110101000001001101001011111011000010100100001010001010000001010011000011001110101100000111000111101011010111100110010101111011011000010101001000110101000000000111011110101111101111100101011110010111001001100001110111010000011101000010011111100100100001011010101011010011101100010110000001110110100111110111001111010111000001010100000101111010100111101001111001010101011111101101101011100001111011011011100001111011110101010000111011110011010011101001100001010000110101100011001110110111001001001001101100111101010111000100010111010101100011110111101011110110111100010100000100111000101000011111110010010101110110010110110000010010111101011010111010011010000010100011100111110100110001001111011101001101101010011000011010000010010110010100111001101101101001100010111010111011100010110011111010011100110000111010010000010011111101011111100001011001000010001001010100110001001000110001001001101101011101100110111111000011010011011111101000101011100110100101000101010111011011100001110111100011100010000011000110000111111011111000011100111110011110010010110101111101110111110010000001101101101000100011110101001000000000010010000110101111100100110001011001111001111101101101011101101001111000001000100011010010011111011000100100011101000000100010000010011011110100100111001001110000100010110000001010000001011011011111011010001100010101100000000010011101111001011110011000001000001000001001110100001100100111000100110010101010011100000001110010110011111010110110011111010000010100011110101010100011101001101011011111111110101110010001011100011000010000101100000101001010101110101011100010010101001011110101110001111000110100000110101100111101";
 	
-   --Inputs
-    signal a : std_logic_vector(63 downto 0) := (others => '0');
-    signal b : std_logic_vector(63 downto 0) := (others => '0');
-	
-	signal s_in_uni, s_in_norm, s_out_norm, s_out_uni : std_logic;
-	
-	signal Sub1Result, Sub1Result_reg : std_logic_vector(63 downto 0) := (others => '0');
+
+   --Inputs	
+	signal Sub1Result : std_logic_vector(63 downto 0) := (others => '0');
 	signal Add1Result : std_logic_vector(63 downto 0) := (others => '0');
 	signal Mult1Result,Mult2Result : std_logic_vector(63 downto 0) := (others => '0');
 	signal Div1Result : std_logic_vector(63 downto 0) := (others => '0');
 	signal Exp1Result : std_logic_vector(63 downto 0) := (others => '0');
 	signal CompResult: std_logic_vector (0 downto 0);
 	signal Mult2Result_Ext, Exp1Result_Ext : std_logic_vector (65 downto 0);
-	signal Shift_in, Shift_out : std_logic_vector(63 downto 0);
+
    signal Proposed_LPR_output : std_logic_vector(63 downto 0);  
     
  	--Outputs
     signal result : std_logic_vector(63 downto 0);
 
-    
+	--PipeLine
+	constant TOTAL_PIPE : integer := 120;
+	constant SMALL_PIPE : integer :=50;
+	signal Proposed_Sample : pipeline_type(1 to TOTAL_PIPE);
+	signal Old_Sample : pipeline_type(1 to TOTAL_PIPE);
+	signal Proposed_LPR : pipeline_type(1 to 50);
+	signal Old_LPR : pipeline_type(1 to 50);
+	signal Old_LPR_input, old_lpr_output, Shift_in_proposed, Shift_out_proposed, Shift_in_old, Shift_out_old : std_logic_vector(63 downto 0);
+	
 	-- Counters
 	signal sample_counter : integer range 0 to 108 := 0;
 	signal load_rng_counter : integer range 0 to 2048 :=0;
 	signal LPR_old_counter : integer range 0 to 1024 := 0;
+	signal Address_Counter : integer range 0 to 8192 :=0;
+	signal Address_Set_Counter : integer range 0 to 1 :=0;  -- Oscillates to give memory address one before
     --Control Signals
 	
 	signal  write_a : std_logic_vector(7 DOWNTO 0);
@@ -80,8 +85,10 @@ end component;
 	signal state,nstate : state_type;
 	
 	-- RNG Signal
-		signal rng_mode_uni, rng_ce_uni, rng_mode_norm, rng_ce_norm: std_logic;
-		signal rng_norm, rng_uni: std_logic_vector(63 downto 0);	
+	signal rng_mode_uni, rng_ce_uni, rng_mode_norm, rng_ce_norm: std_logic;
+	signal rng_norm, rng_uni: std_logic_vector(63 downto 0);	
+	signal s_in_uni, s_in_norm, s_out_norm, s_out_uni : std_logic;
+	
 begin
 	-- RNG + Xi
 	-- 12 cycles
@@ -198,26 +205,32 @@ begin
 				load_rng_counter <= 0;
 			elsif activate='1' then
                 -- Place output in shift register for use later
-				shift_in <= Add1Result;
+				shift_in_proposed <= Add1Result;
             if state = load_rng then
 					load_rng_counter <= load_rng_counter + 1 ;
 				else
+					-- Pipeline old sample incase its needed
+					Old_Sample(1) <= xState;
+					Old_Sample(1 to TOTAL_PIPE-1) <= Old_sample(2 to TOTAL_PIPE);
+					Shift_out_old <= Old_sample(TOTAL_PIPE);
 					-- Shifting of proposed value to end of pipeline
-					Shift_out <= Proposed_sample(120);
-					Proposed_sample(1 to 119) <= Proposed_sample(2 to 120);
-					Proposed_sample(1) <= Shift_in;
+					Shift_out_proposed <= Proposed_sample(TOTAL_PIPE);
+					Proposed_sample(1 to TOTAL_PIPE-1) <= Proposed_sample(2 to TOTAL_PIPE);
+					Proposed_sample(1) <= Shift_in_proposed;
 					-- LPR Value pipeline 
 					Proposed_LPR(1) <= Div1Result;
 					Proposed_LPR(1 to 49) <= Proposed_LPR(2 to 50);	
 					Proposed_LPR_output <= Proposed_LPR(50);
-					
-					if sample_counter < 108 then
+					-- Pipe of previous LPR value
+					Old_LPR(1) <= data_out_b;
+					Proposed_LPR(1 to 49) <= Proposed_LPR(2 to 50);	
+					Old_LPR_output <= Proposed_LPR(50);
+					if sample_counter < TOTAL_PIPE then
 						sample_counter <= sample_counter + 1;
-					end if;
-                    if sample_counter > 220 then
-                        LPR_old_counter <= lpr_old_counter + 1;
-                    end if;
-                end if;
+					else 
+						Address_Counter <= Address_Counter + 1;
+				   end if;
+            end if;
 			end if;
 			
 		end process Control_sync;
@@ -265,22 +278,28 @@ begin
 						s_in_norm <= seed(load_rng_counter);
 					end if;	
 				when running =>
-					nstate <= running;
-                    if sample_counter > 220 then
-                        if LPR_old_counter =  10 then --Must change one before value needed
-                            addr_b <= x"00000000";
-                        end if;
-                    end if;    
-                    --Store sub1Result for a cycle because of memory latency
-                    Sub1Result_reg <= Sub1Result;
-					if sample_counter = 108 then
+					nstate <= running; 
+					
+					if sample_counter = 69 then
+						addr_b <= std_logic_vector(to_unsigned(Address_Counter - VARS,addr_b'length));
+					elsif sample_counter >70 then
+						addr_b <= std_logic_vector(to_unsigned(Address_Counter - VARS,addr_b'length));
+						Old_LPR_input <=data_out_b;
+					end if;
+					
+					if sample_counter = 119 then -- Needs to be addressed one step before
+						write_a <= x"11";
+                  addr_a <= std_logic_vector(to_unsigned(Address_Counter,addr_a'length));
+					elsif sample_counter = 120 then
+					      write_a <= x"11";
+                     addr_a <= std_logic_vector(to_unsigned(Address_Counter,addr_a'length));
 						if CompResult = "1" then
-						-- Save to address and forward to next LPR unit
-                            write_a <= x"11";
-                            addr_a <= x"00000000";
-                            -- Pipe needs to be one step longer to allow for address change
-                            data_in_a <= Shift_out;
-							Output <= Proposed_LPR_output;
+						-- Save to LPR address and X forward to next LPR unit
+						   data_in_a <= Proposed_LPR_output;
+							Output <= Shift_out_proposed;
+						else
+							data_in_a <= Old_LPR_output;
+							Output <= Shift_out_old;
 						end if;
 					end if;
 			end case;
