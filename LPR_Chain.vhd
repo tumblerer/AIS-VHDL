4a8db1405ff6b8990c2bfe5931135849d0341d4d
Chris <christory@hotmail.com>
2013-03-12 23:00:34 +0000

    Added BRAm to save final states, began shift reg loop back

--- LPR_Chain.vhd
+++ LPR_Chain.vhd
@@ -2,7 +2,8 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 library work;
 use work.Pack.all;
-
+use work.beta_pack.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity LPR_Chain is
   port (
@@ -46,12 +47,19 @@ end component;
   signal Mem_Data_B_In : wire_array;
 	signal Mem_Addr_B_In :mem_addr_wire;
   signal activate_in : std_logic;
-	--signal Beta : std_logic_vector(STATE_SIZE downto 0);
+	signal Beta_in : std_logic_vector(STATE_SIZE downto 0);
 	signal activate_gen: std_logic;
   signal seed : std_logic_vector (127 downto 0);
+  signal Loop_Back_Pipe : pipeline_type (1 to STEPS*RUNS-TOTAL_PIPE*BLOCKS);
 
-  signal counter: integer range 0 to 2050;
+  -- BRAM
+  signal addr_a, addrb : std_logic_vector(31 downto 0);
+  signal write_a : std_logic_vector(7 downto 0);
+  signal doutb : std_logic_vector(63 downto 0);
 
+  --Counter
+  signal counter: integer range 0 to RUNS*STEPS+1;
+  signal address_counter : integer range 0 to RUNS*STEPS*8;
 begin
 
   Gen:  entity work.Generate_Sample Port Map(
@@ -62,12 +70,24 @@ begin
           sample_output => X_wire(0)
         );
 
-  Chain: for i in 1 to 2 generate
+  -- BRAM for all the final X states found
+BRAM_X: ENTITY work.Dual_Port_BRAM PORT MAP(
+       clka => clk,
+       wea => write_a,
+       addra => addr_a,
+       dina => X_wire(BLOCKS),
+       clkb => clk,
+       rstb => reset,
+       addrb => addrb,
+       doutb => doutb
+  );
+
+  Chain: for i in 1 to BLOCKS generate
   begin
     LPR_TOP0: entity work.LPR_top Port Map (
          clk => clk,
          reset => reset,
-         Beta => x"3f747ae147ae147b",
+         Beta => beta(i),
          activate_in => activate_wire(i-1),
          activate_out => activate_wire(i),
          X_In => X_wire(i-1),
@@ -82,6 +102,7 @@ begin
 
   seed <= x"0123456789abcdef0123456789abcdef";
   Mem_Data_B_In(1) <= (Others => '0');
+  
   Control : process
   begin
     wait until clk'EVENT AND clk='1';
@@ -89,9 +110,11 @@ begin
         activate_gen <= '0';
         activate_wire(0) <= '0';
         counter <= 0;
+        address_counter <= 0;
+        addr_a <= std_logic_vector(to_unsigned(0,addr_a'length));
       else
+        write_a <= x"FF";
         if counter < 2100 then
-          counter <= counter + 1;
           activate_wire(0) <='0';
         else 
          activate_wire(0) <= '1';
@@ -102,7 +125,13 @@ begin
         else
           activate_gen <= '0';
         end if;
-         
+
+        if counter < STEPS*RUNS  then
+          counter <= counter + 1;
+        else
+          address_counter <= address_counter + 8;
+          addr_a <= std_logic_vector(to_unsigned(address_counter,addr_a'length));
+        end if;  
       end if;
 
 
