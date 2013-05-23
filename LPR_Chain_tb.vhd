c64723ee5613d4decd737e47dfc3864ec8473016
Chris <christory@hotmail.com>
2013-05-23 15:19:53 +0100

    Added compelete condition to testbench

--- LPR_Chain_tb.vhd
+++ LPR_Chain_tb.vhd
@@ -26,7 +26,8 @@ ARCHITECTURE behavior OF LPR_Chain_tb IS
   		  wea_seed : in std_logic_vector(7 downto 0);
   		  wea_beta : in std_logic_vector(7 downto 0);
         addrb_X : in std_logic_vector(31 downto 0);
-        doutb_x : out  std_logic_vector(63 downto 0)
+        doutb_x : out  std_logic_vector(63 downto 0);
+        complete: out std_logic
    ) ;
     END COMPONENT;
     
@@ -42,6 +43,7 @@ ARCHITECTURE behavior OF LPR_Chain_tb IS
    signal addra_seed : std_logic_vector(31 downto 0);
    signal addrb_x : std_logic_vector(31 downto 0);
    signal doutb_x : std_logic_vector(63 downto 0);
+   signal complete : std_logic;
    -- Clock period definitions
    constant clk_period : time := 10 ns;
 
@@ -60,7 +62,8 @@ BEGIN
           wea_beta => wea_beta,
           wea_seed => wea_seed,
           addrb_x => addrb_x,
-          doutb_x => doutb_x
+          doutb_x => doutb_x,
+          complete => complete
         );
 
    -- Clock process definitions
@@ -94,34 +97,6 @@ BEGIN
         addra_beta <= std_logic_vector(to_unsigned(addr_count,addra_beta'length));
         wait for clk_period;
       end loop;
-      -- Load memory
-      -- 0.0
-      -- addra_beta <= std_logic_vector(to_unsigned(0,addra_beta'length));
-      -- addra_seed <= std_logic_vector(to_unsigned(0,addra_seed'length));
-      -- wait for clk_period;
-      -- dina_beta <= "0011111110100000000000000000000000000000000000000000000000000000";
-      -- dina_seed <= "0011111110100000000000000000000000000000000000000000000000000000";
-      -- wait for clk_period;
-      
-      -- -- 0.5
-      -- addra_beta <= x"00000008";
-      -- addra_seed <= x"00000008";
-      -- wait for clk_period;
-      -- dina_beta <= "0011111111000000000000000000000000000000000000000000000000000000";
-      -- dina_seed <= "0011111111000000000000000000000000000000000000000000000000000011";
-
-      -- wait for clk_period;
-      -- addra_beta <= x"00000010";
-      -- addra_seed <= x"00000010";
-      -- dina_beta <=  "0011111111010000000000000000000000000000000000000000000000000000";
-      -- -- 1.0
-      -- wait for clk_period;
-      -- addra_beta <= x"00000018";
-      -- addra_seed <= x"00000018";
-      -- wait for clk_period;
-      -- dina_beta <= "0011111111100000000000000000000000000000000000000000000000000000";
-      -- dina_seed <= "0011111111100000000000000000000000000000000000000000000000000011";
-      -- -- insert stimulus here 
       wait for clk_period;
 
       seed_load : for i in 1 to 100 loop
@@ -133,8 +108,14 @@ BEGIN
          dina_seed <= "0011111111100000000000000000000000000000000000000000000000000000";
       end loop ; -- seed_load
       reset <= '0';
+
       wait for clk_period*100;
-      wait;
+      while true loop
+        assert complete = '0'
+          report "Simultion stopped at completion"
+          severity FAILURE;
+        wait for clk_period;
+      end loop;
    end process;
 
 END;
